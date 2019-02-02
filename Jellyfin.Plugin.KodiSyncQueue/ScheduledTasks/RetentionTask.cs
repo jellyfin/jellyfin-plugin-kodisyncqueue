1de98575bada1fd2c486d93f69d2e0af5c2eff0c
Claus Vium <clausvium@gmail.com>
2019-02-02 13:02:24 +0100

    More log cleaning

--- Jellyfin.Plugin.KodiSyncQueue/ScheduledTasks/RetentionTask.cs
+++ Jellyfin.Plugin.KodiSyncQueue/ScheduledTasks/RetentionTask.cs
@@ -20,7 +20,7 @@ namespace Jellyfin.Plugin.KodiSyncQueue.ScheduledTasks
         public FireRetentionTask(ILogger logger)
         {
             _logger = logger;
-            _logger.LogInformation("Jellyfin.Plugin.KodiSyncQueue.Task: Retention Task Scheduled!");
+            _logger.LogInformation("Retention Task Scheduled!");
         }
 
         public string Key => "KodiSyncFireRetentionTask";
@@ -40,14 +40,8 @@ namespace Jellyfin.Plugin.KodiSyncQueue.ScheduledTasks
         public async Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
         {
             //Is retDays 0.. If So Exit...
-            if (!int.TryParse(Plugin.Instance.Configuration.RetDays, out var retDays)) {
-                _logger.LogInformation("Jellyfin.Plugin.KodiSyncQueue.Task: Retention Deletion Not Possible When Retention Days = 0!");
-                return;
-            }
-
-            if (retDays == 0)
-            {
-                _logger.LogInformation("Jellyfin.Plugin.KodiSyncQueue.Task: Retention Deletion Not Possible When Retention Days = 0!");
+            if (!int.TryParse(Plugin.Instance.Configuration.RetDays, out var retDays) || retDays == 0) {
+                _logger.LogInformation("Retention Deletion Not Possible When Retention Days = 0!");
                 return;
             }
 
@@ -65,9 +59,7 @@ namespace Jellyfin.Plugin.KodiSyncQueue.ScheduledTasks
         }
 
         public string Name => "Remove Old Sync Data";
-
         public string Category => "Jellyfin.Plugin.KodiSyncQueue";
-
         public string Description => "If Retention Days > 0 then this will remove the old data to keep information flowing quickly";
     }
 
