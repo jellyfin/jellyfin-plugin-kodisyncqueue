15930bc2c05d6ae82c9de958422647d1403de890
grafixeyehero <32230989+grafixeyehero@users.noreply.github.com>
2021-05-21 05:23:37 +0300

    revert back XmlElement

--- Jellyfin.Plugin.KodiSyncQueue/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.KodiSyncQueue/Configuration/PluginConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.KodiSyncQueue.Configuration
@@ -19,14 +20,19 @@ namespace Jellyfin.Plugin.KodiSyncQueue.Configuration
 
         public bool IsEnabled { get; set; }
 
+        [XmlElement("tkMovies")]
         public bool TkMovies { get; set; }
 
+        [XmlElement("tvTVShows")]
         public bool TkTvShows { get; set; }
 
+        [XmlElement("tkMusic")]
         public bool TkMusic { get; set; }
 
+        [XmlElement("tkMusicVideos")]
         public bool TkMusicVideos { get; set; }
 
+        [XmlElement("tkBoxSets")]
         public bool TkBoxSets { get; set; }
     }
 }
